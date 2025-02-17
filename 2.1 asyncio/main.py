631e94b0549a5afcb801b9741173223b95203a60
Ivan <gordeevid@yandex.ru>
2025-02-17 21:22:24 +0300

    2.2 added async request at lower levels

--- 2.1 asyncio/main.py
+++ 2.1 asyncio/main.py
@@ -19,55 +19,84 @@ TIME_SLEEP_REQUESTS = int(os.getenv('TIME_SLEEP_REQUESTS', 0))
 URL = os.getenv('URL')
 
 
-async def get_json(session:ClientSession, url:str)->dict:
-    
-    result = await session.get(f'{url}')
-    if result.status == 200:
-        return await result.json() 
-    else:    
+async def get_json(session:ClientSession, url_list:list[str], tag:str = None)->list[dict]:
+    result = []
+    if type(url_list) == str:
+        url_list = [url_list]
+    for url in url_list:
+        await asyncio.sleep(TIME_SLEEP_REQUESTS) 
+        response = await session.get(f'{url}')
+        if response.status == 200:
+            if not tag:
+                result.append(await response.json())
+            else:
+                json_resp = await response.json()
+                result.append(json_resp.get(tag))
+        else:    
+            result.append({})
+    return result if len(result) > 1 else result[0] if len(result) != 0 else []
+
+
+
+async def get_data_toDB(session:ClientSession, people_id: int):
+
+    data = await get_json(session=session, url_list=f'{URL}people/{people_id}')
+    if not data:
         return {}
-
-
-
-async def get_data_toDB(people_id: int):
-    async with ClientSession() as session:
-        data = await get_json(session=session, url=f'{URL}people/{people_id}')
-        if data == {}:
-            return {}
-        new_data = {
-            'id':people_id,
-            'birth_year':data.get('birth_year'),
-            'eye_color':data.get('eye_color'),
-            'films':[await get_json(session=session, url = film) for film in data.get('films')],
-            'gender':data.get('gender'),
-            'hair_color':data.get('hair_color'),
-            'height':data.get('height'),
-            'homeworld':data.get('homeworld'),
-            'mass':data.get('mass'),
-            'name':data.get('name'),
-            'skin_color':data.get('skin_color'),
-            'species':[await get_json(session=session, url = film) for film in data.get('species')],
-            'starships':[await get_json(session=session, url = film) for film in data.get('starships')],
-            'vehicles':[await get_json(session=session, url = film) for film in data.get('vehicles')]
-        }
-        print('\n\n')
-        print(new_data)
-    # insert_people_corut = insert_people(result)
+    tasks = []
+    
+    tasks.append(get_json(session=session, url_list=data.get('films'), tag='title'))
+    tasks.append(get_json(session=session, url_list=data.get('species'), tag='name'))
+    tasks.append(get_json(session=session, url_list=data.get('starships'), tag='name'))
+    tasks.append(get_json(session=session, url_list=data.get('vehicles'), tag='name')) #TODO: add homeworld with tag 'name'
+    
+    returns = await asyncio.gather(*tasks)
+    
+    if returns:
+        films = returns[0]
+        species = returns[1]
+        starships = returns[2]
+        vehicles = returns[3]
+    else:
+        films = []
+        species = []
+        starships = []
+        vehicles = []
+        
+    new_data = {
+        'id':people_id,
+        'birth_year':data.get('birth_year'),
+        'eye_color':data.get('eye_color'),
+        'films':films,
+        'gender':data.get('gender'),
+        'hair_color':data.get('hair_color'),
+        'height':data.get('height'),
+        'homeworld':data.get('homeworld'),
+        'mass':data.get('mass'),
+        'name':data.get('name'),
+        'skin_color':data.get('skin_color'),
+        'species':species,
+        'starships':starships,
+        'vehicles':vehicles
+    }
+    print('\n\n')
+    print(new_data)
+# insert_people_corut = insert_people(result)
     # task = asyncio.create_task(insert_people_corut)
 
 async def main():
-    
-    for people_id in range(1, MAX_PEOPLE_ID):
-        task = asyncio.create_task(get_data_toDB(people_id = people_id))
-        print('add task___________________________________________________')
-        await asyncio.sleep(TIME_SLEEP_REQUESTS)
+    async with ClientSession() as session:
+        for people_id in range(1, MAX_PEOPLE_ID):
+            task = asyncio.create_task(get_data_toDB(session = session, people_id = people_id))
+            print('add task___________________________________________________')
+            
 
-    tasks = asyncio.all_tasks()
-    tasks.remove(asyncio.current_task())
-    print('End Program\nFINISH ALL TASKS_____________________________________')
-    if tasks != None:
-        for task in tasks:
-            await task
+        tasks = asyncio.all_tasks()
+        tasks.remove(asyncio.current_task())
+        print('End Program\nFINISH ALL TASKS_____________________________________')
+        if tasks != None:
+            for task in tasks:
+                await task
             
     print('Done!')
 
